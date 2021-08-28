0794d2fdb81780e61bea7ecc4ccac08e1a313301
DmytroBerezovskyi <berezovscky.dmitrij1@gmail.com>
2021-08-28 11:36:12 +0300

    HomeWork2 - Escape символы

---
+++ Homework2.py
@@ -0,0 +1,7 @@
+print('Escape sequences:','\n','\t' '              \ a''            Bell (alert)',
+'\n''\t''              \ b''            Backspace',
+'\n''\t''              \ n''            New Line',
+'\n''\t''              \ t''            Horizontal tab',
+'\n''\t''              \ \ ''           Backslash \ ',
+'\n''\t''              \ " ''           Double quotation mark " ',
+'\n''\t''              \ '"'" '            Single quotation mark'"'")
