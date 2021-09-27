881eb5562656fd9780b84cd72054ab5cf97165c0
DmytroBerezovskyi <berezovscky.dmitrij1@gmail.com>
2021-09-27 18:14:16 +0300

    With diff backslash

--- Homework2.py
+++ Homework2.py
@@ -1,9 +1,9 @@
-print('Escape sequences:','\n','\t\t\t\t' r'\a' "            Bell (alert)",
-"\n \t\t\t\t"              r'\b'"            Backspace",
-"\n \t\t\t\t"              r'\n'"            New Line",
-"\n \t\t\t\t"              r'\t' "            Horizontal tab",
-"\n \t\t\t\t"              r'\\'"            Backslash \ ",
-"\n \t\t\t\t"              r'\" '"           Double quotation mark"
-"\n \t\t\t\t"              r'\' ' "           Single quotation mark" "'")
+print('Escape sequences:''\n''\t\t\t\t' "\\a              Bell (alert)",
+"\n \t\t\t\t"              "\\b              Backspace",
+"\n \t\t\t\t"             "\\n              New Line",
+"\n \t\t\t\t"              "\\t              Horizontal tab",
+"\n \t\t\t\t"              "\\\              Backslash \ ",
+"\n \t\t\t\t"              '\\"'  "              Double quotation mark "'"'
+"\n \t\t\t\t"              "\\'              Single quotation mark" " '")
 
 
